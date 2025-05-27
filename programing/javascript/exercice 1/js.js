1884579ea9f7f6a108c69436fceb7ee70d2816a4
Tales094 <al.barbe@hotmail.fr>
2025-05-27 13:59:47 +0200

    exo2

--- programing/javascript/exercice 1/js.js
+++ programing/javascript/exercice 1/js.js
@@ -3,10 +3,113 @@ const majeur =18;
 console.log(30 < majeur)
 
 
-let age2= 25;
+let age2= 25;             /* préfairable*/
 const majeur2 =18;
 console.log(age > majeur)
 
 let age3=12;
 const majeur3=18;
 console.log(12 < 18)
+
+
+
+
+
+
+/*Exercice 2*/
+
+/*addition*/
+let number =3;              /*1er façon*/
+let number2 =5;
+let resultat= number+number2;
+console.log(resultat);
+
+
+console.log(number+number2);    /*2eme façon*/
+
+
+/*soustraction*/
+let number3 = 10;
+let number4 = 7;
+let resultat2 = number3-number4
+console.log(resultat2);
+
+console.log(number3-number4);
+
+/*multiplication*/
+let number5 = 4;
+let number6 = 6;
+let resultat3 = number5*number6;
+console.log(resultat3);
+
+console.log(number5*number6);
+
+/*Division*/
+let number7 = 24;
+let number8 = 6;
+let resultat4 = number7/number8;
+console.log(resultat4);
+
+console.log(number7/number8);
+
+/*Pourcentage*/
+let number9 = 30;
+let number10 = 5;
+let resultat5 = number9%number10
+console.log(resultat5);
+
+console.log(number9%number10);
+
+/*puissance*/
+let number11 = 100;
+let number12 = 25;
+let resultat6 = number11**number12;
+console.log(resultat6);
+
+console.log(number11**number12);
+
+
+
+/*Exercice 3*/
+
+/*Exemple*/ 
+// let text = "bonjour";
+// console.log(text);
+
+// text = text + ' je m\'appelle'
+
+// console.log(text);
+
+// let textt = aurevoir;
+// textt + textt
+
+//  $[textt], " tout le monde"
+
+/*Exercice*/
+
+/*-1 :*/
+let prenom = "John";
+let nom = "Doe";
+let nomComplet = prenom +" "+ nom;
+
+console.log(nomComplet);
+
+
+
+/*-2*/
+
+let sujet = "Le Chat" ;
+let verbe = "mange" ;
+let ojbet = "la souris" ;
+ let phrase = `${sujet} ${verbe} ${ojbet}`
+
+
+
+console.log(phrase); 
+
+console.log("Text :", phrase);
+
+
+
+
+
