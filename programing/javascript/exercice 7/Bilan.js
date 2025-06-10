c5f3c64e5f39d4d36539ed1aff90f1fa1b6bffd6
Tales094 <al.barbe@hotmail.fr>
2025-06-10 08:35:33 +0200

    save

---
+++ programing/javascript/exercice 7/Bilan.js
@@ -0,0 +1,135 @@
+
+
+
+
+
+
+
+
+
+
+
+                                // Type de variable
+
+// Undefined (non defini=aucune affectation de valeur'pas )
+// Number
+// String (chaine de caratére) = ""
+// Object [ Dans lacolab valeur + clef] let obj = {nom =}
+// Boolean
+// Arrray-- (en réalité, ce sont des objets) ex: let tab = []
+// Null ()
+
+                            // Changer les paramétre du DOM
+
+        // 1 - Selectionner l'élément
+//  ex: const elementSelectionne = document.querySelector('.cal_instruction')
+//  elementSelectionne.backgroundColor=red
+
+        // 2 - Choisir la propriété a modifier (tjr la meme chose)
+        // 3 - Afficher la nouvelle valeur de la propriété
+ // function
+    //  delcaration ex: function demoin(){
+    //     console.log("");    
+    //  }
+    //  appel function ex: demo()
+
+    //  function demoAvecParam(param){
+    //     console.log(param);
+    //     return (15)
+    //  }
+
+    //  demoAvecParam()
+
+                                // if
+//         ex:let demonVarialbe =30
+//         let vari 
+//         if (demonVarialbe){
+//             console.log("si demon variable existe");
+            
+//         } 
+//         else{
+//             if (vari > 10){
+//             console.log("variable supérieur a 10");
+//             Truc();    
+//             }
+//             else{
+//                 console.log("varible inférieur a 10");
+                
+//             }
+//             console.log("si elle existe pas on fait sa");
+            
+//         }
+
+    
+// }
+
+// ==
+// ===
+// !==
+// >
+// <
+// >=
+// ...
+
+ 
+                            //else
+ // for = (boucle)
+// let tab = [1,2,5,7,9]
+
+//  for (let index = 0; index < array.length; index++){
+//     const element = array[index]; (Affiche le contenu par rapport a la position)
+//  }
+
+//  for (let index = 0; index < 5; index++){
+//     console.log(tab[index]);
+    
+//  }
+
+                            // while
+
+//  while (condition) {
+    
+//  }
+ //objet
+ //Array = (Tableau)
+
+// ex:
+// let obj = "posotion dan la mémoire"
+
+//  let tab = [1,2,3,4]
+//  console.log(tab);
+
+//  let ojb = {
+//     nom : "",
+//     prenom : "",
+//     age : 36,
+//  }
+
+ 
+
+//  let obj2 = ojb
+
+//  obj.age = obj+1
+//  obj.age = obj.age + 1
+
+//  console.log(ojb);
+//  console.log(ojb2);
+  let tab = [1,2,3,4,5]
+let tab2 = [...tab]
+
+ttab[2] = 10
+
+console.table(tab);
+console.table(tab2)
+
+
+                            //  Ecouter d"évenement
+
+//                             selection un element : 
+//                             on va dire dans la page dek un element eun selection on va effectuer une action
+
+// const rectangle = document.querySelector('click'), ( => {
+//     console.log(("jaffiche un trcu dans la console"));
+
+    
+// })
