fb6f6df9d82261f1951e42445049f7f876f36bc6
Tales094 <al.barbe@hotmail.fr>
2025-06-13 16:31:33 +0200

    pendu

---
+++ programing/javascript/exercice 13/jsSwitch.js
@@ -0,0 +1,14 @@
+let age = 2
+
+
+
+switch (age) {
+    case 1: case 2: case 4: case 7: case 8: case 10:
+        console.log("31 jours");
+        
+        break;
+        default:
+            console.log("je ne fait rien");
+            
+            break;
+}
